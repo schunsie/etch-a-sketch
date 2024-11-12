c7c25ef3d382993b2e0e7c782b9b309be3cf61aa
schunsie <185822585+schunsie@users.noreply.github.com>
2024-11-12 22:13:27 +0100

    Add functionality of rainbow paint mode

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Etch-a-sketch grid
 const grid = document.querySelector('#grid'); 
 
 let gap = 2;
@@ -31,6 +32,28 @@ grid.addEventListener('mouseover', (e) => {
     target = e.target;
 
     if (target.classList.contains('grid-block')) {
-        target.style.backgroundColor = 'black';
+        target.style.backgroundColor = `${getPaintColor()}`;
     }
 });
+
+
+// Options menu
+const colorSwitch = document.querySelector('#color-switch')
+let paintColor = 'black';
+let rainbowMode = false; 
+
+colorSwitch.addEventListener('click', () => {
+    if (colorSwitch.checked) rainbowMode = true;
+    else rainbowMode = false;
+});
+
+function getPaintColor() {
+    if (rainbowMode) {
+        const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+        const r = randomBetween(0, 255);
+        const g = randomBetween(0, 255);
+        const b = randomBetween(0, 255);
+        return `rgb(${r},${g},${b})`;
+    }
+    else return 'black';
+}
