ea4f362cf282032baa5904bab2386d0f7bec5f34
schunsie <185822585+schunsie@users.noreply.github.com>
2024-11-12 11:34:53 +0100

    Add JS for 16x16 grid + styling

---
+++ script.js
@@ -0,0 +1,26 @@
+const grid = document.querySelector('#grid');
+
+let gap = 2;
+let gridSize = 16;
+let blockSize = calcSize(gridSize, grid.clientWidth);
+
+for (let i = 1; i <= gridSize; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    grid.appendChild(row);
+
+    for (let j = 1; j <= gridSize; j++) {
+        const gridBlock = document.createElement('div');
+        gridBlock.classList.add('grid-block');
+        
+        gridBlock.setAttribute('style', `width: ${blockSize}; height: ${blockSize};`);
+        
+        row.appendChild(gridBlock);
+    }
+}
+
+function calcSize(gridSize, gridWidth) {
+    // All sizes in pixels
+    let pixels = (gridWidth - gap * (gridSize-1)) / gridSize;
+    return `${pixels}px`;
+}
